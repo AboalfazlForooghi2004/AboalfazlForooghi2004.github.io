e7ee8b7412a220c04de6dad529b8e1ddadaa1f12
A.Forooghi <96820237+AboalfazlForooghi2004@users.noreply.github.com>
2022-03-19 02:03:03 +0330

    teste2

--- js/js.js
+++ js/js.js
@@ -1,1 +1,7 @@
 // JavaScript Document
+
+/*start trigger*/
+let el = document.querySelector('.el');
+let height = el.scrollHeight;
+el.style.setProperty('--max-height', height + 'px');
+/*end trigger*/
