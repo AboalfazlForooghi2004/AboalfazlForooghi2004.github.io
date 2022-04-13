2655def82e1a3c95c78933b226985cf44576a3fc
A.Forooghi <96820237+AboalfazlForooghi2004@users.noreply.github.com>
2022-04-13 16:28:53 +0430

    Add8

--- js/js.js
+++ js/js.js
@@ -1,7 +1,9 @@
 // JavaScript Document
 
+
 const typeWriter = document.getElementById('typewriter-text');
-const text = 'سلام،ابوالفضل فروغی هستم به سایت من خوش امدید.';
+
+const text = 'سلام ابوالفضل فروغی هستم  به سایت من خوش آمدید....';
 
 typeWriter.innerHTML = text;
 typeWriter.style.setProperty('--characters', text.length);
